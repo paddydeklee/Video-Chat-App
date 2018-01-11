fdf50550d61b75411a86b2dcc7f9c3596dd77c47
Paddy de Klee <paddydeklee@gmail.com>
2018-01-11 12:59:56 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,39 @@
+"use srtict";
+
+// 1 require packages
+var http = require("http"),
+	express = require("express");
+
+// 1 create 
+const app = express();
+app.set("view engine", "jade");
+
+// 2 middleware pipeline
+app.use((request, response, next) =>{
+	console.log("In middleware 1");
+	next();
+	console.log("out of middleware 1");
+});
+
+app.use(express.static("./public"));
+
+app.use((request, response, next) => {
+	console.log("---- In middleware 2");
+	next();
+	console.log("---- out of middleware 2");
+});
+
+// 2 end response
+app.get("/", (request, response) => {
+	response.end("Hello world");
+});
+
+// 1 listen on server - check server is listening
+const server = new http.Server(app);
+
+const port = 3000;
+server.listen(port, () => {
+	console.log("server started on port " + port);
+});
+
+// 2 use template engine
